e8fdc06c5bfb45bb8a11f095c24432da81858472
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-07-19 08:51:16 -0400

    Fix error messages

--- api/src/controllers/activities.js
+++ api/src/controllers/activities.js
@@ -5,13 +5,15 @@ exports.postActivity = async (req,res) => {
     const { name,  difficulty, duration, season, countries} = req.body
     if (!name || !difficulty || !duration || !season || !countries.length){
         return res.status(400).json({
-            error: "name, difficulty, duration, season and countries cannot be empty",
-            values: {
-                name,
-                difficulty,
-                duration,
-                season,
-                countries
+            error: {
+                msg: "name, difficulty, duration, season and countries cannot be empty",
+                values: {
+                    name,
+                    difficulty,
+                    duration,
+                    season,
+                    countries
+                }
             }
         })
     }
@@ -20,10 +22,10 @@ exports.postActivity = async (req,res) => {
         difficulty,
         duration,
         season
-    }).catch(e => console.log(e))
+    }).catch(e => res.status(500).json(e))
     countries.map( async c => {
         const country = await Country.findOne({ where: { country_id: c } })
-        .catch(e => console.log(e))
+        .catch(e => res.status(500).json(e))
         activity.addCountry(country)
     })
     return res.json(activity)
