51fca8cdf30cc9bbf64cc49d8b5178ad7fe5022e
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-07-21 23:21:48 -0400

    Add controller to add activities to countries

--- api/src/controllers/countries.js
+++ api/src/controllers/countries.js
@@ -37,6 +37,27 @@ exports.getCountryById = async (req,res) => {
     return res.json(country)
 }
 
+exports.addActivityToCountry = async (req,res) => {
+    const { a_id, c_id } = req.params
+    const country = await Country.findByPk(c_id, {include: [Activity]})
+    if (country === null) return res.status(404).json({
+        error : {
+            message: "Country doesn't exist",
+            values: {c_id,a_id}
+        }
+    })
+    const activity = await Activity.findByPk(c_id)
+    if (activity === null) return res.status(404).json({
+        error : {
+            message: "Activity doesn't exist",
+            values: {c_id,a_id}
+        }
+    })
+    await country.addActivity(activity)
+    const newData = await Country.findByPk(country.country_id, {include: [Activity]})
+    return res.status(201).json(newData)
+}
+
 exports.removeActivityFromCountry = async (req, res) => {
     const { c_id, a_id } = req.params
     const activity = await Activity.findByPk(a_id)
