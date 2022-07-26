2d62c985cb6f8768520d04f07b1e8ee43a0651eb
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-07-25 22:52:50 -0400

    Add details to country detail

--- client/src/components/CountryDetail.js
+++ client/src/components/CountryDetail.js
@@ -1,9 +1,16 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../index.css'
 
 const CountryDetail = (props) => {
   return (
-    <div style={{border:'1px solid red'}}>CountryDetail {props.id}</div>
+    <div style={{border:'1px solid red'}}>
+      <p>Hola, mundo!</p>
+      <p>CountryDetail {props.match.params.id}</p>
+      <Link to='/home' className='btn btn-enter'>Volver a home</Link>
+    </div>
   )
 }
 
