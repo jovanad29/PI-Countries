9b83ba97e75f4e626d76b7d83ea09202559456c6
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-07-27 23:48:00 -0400

    Add useEffect

--- client/src/components/CountryDetail.js
+++ client/src/components/CountryDetail.js
@@ -1,15 +1,30 @@
 
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useParams } from 'react-router-dom'
+import { getCountryDetail } from '../actions'
 
 import '../index.css'
 
-const CountryDetail = (props) => {
+const CountryDetail = () => {
+  const { id } = useParams()
+  const dispatch = useDispatch()
+  useEffect( () => {
+    console.log(id)
+    dispatch(getCountryDetail(id))
+  },[dispatch, id])
+  const country = useSelector(store => store.country)
   return (
     <div style={{border:'1px solid red'}}>
-      <p>Hola, mundo!</p>
-      <p>CountryDetail {props.match.params.id}</p>
-      <Link to='/home' className='btn btn-enter'>Volver a home</Link>
+      <h1>CountryDetail</h1>
+      <p>{country.country_id}</p>
+      <p>{country.name}</p>
+      <p>{country.area}km2</p>
+      <p>{country.population}</p>
+      <ul>{country.activities?.map(a => {
+        return <li key={a.activity_id}>{a.name}</li>
+      })}</ul>
+      <Link to='/home' className='btn btn-enter'>Volver a home</Link> {/* goBack? */}
     </div>
   )
 }
