dad022968ea794a9e8a7729afd01ae17208c7039
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-07-31 19:10:55 -0400

    Add navbar and filters

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,21 +1,47 @@
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCountries } from '../actions'
+import NavBar from './NavBar'
 import CountryCard from './CountryCard'
+import SearchBar from './SearchBar'
+import '../assets/css/Home.css'
+import Sort from './Sort'
+import Filters from './Filters'
+import axios from 'axios'
 
 const Home = () => {
-  const countries = useSelector(state => state.countries)
+  const allCountries = useSelector(state => state.countries)
   const dispatch = useDispatch()
+  const [countries, setCountries] = useState([])
   useEffect(() => {
     dispatch(getCountries())
   },[dispatch])
+  const onSearch = async (searched) => {
+    const { data } = await axios.get('http://localhost:3001/countries?name='+searched) 
+    setCountries(data)
+  }
   return (
-    <div className='cards-container'>
-        {countries?.map((c, idx) => {
-            return <CountryCard key={idx} id={c.country_id} />
-        })}
-    </div>
+    <>
+      <NavBar />
+      <div className='container'>
+        <div className='filters-container'>
+          <SearchBar onSearch={onSearch}/>
+          <Sort />
+          <Filters />
+        </div>
+        <div className='cards-container'>
+            {countries.length ?
+              countries.map((c) => {
+                return <CountryCard key={c.country_id} {...c} />
+              }).slice(0,9) :
+              allCountries.map((c) => {
+                return <CountryCard key={c.country_id} {...c} />
+              }).slice(0,9)
+            }
+        </div>
+      </div>
+    </>
   )
 }
 
