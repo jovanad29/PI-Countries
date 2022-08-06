d0511f73276a0bffd496c970ff00d0b10e639814
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-05 20:09:23 -0400

    Add styles

--- client/src/components/CountryCard.js
+++ client/src/components/CountryCard.js
@@ -2,16 +2,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../index.css'
+import styles from '../assets/css/CountryCard.module.css'
 
 
 const CountryCard = (props) => {
   const {country_id, name, continent, flag_img} = props
   return (
-    <div className='cards'>
-      <p>{name}</p>
-      <img src={flag_img[1]} alt='country flag' width='150px' />
+    <div className={styles.card}>
+      <p>{name.length > 19 ? name.slice(0,20)+'...' : name}</p>
+      <img src={flag_img[1]} alt='country flag' />
       <p>{continent}</p>
-      <Link to={`/country/${country_id}`} className='btn btn-outline'>More</Link>
+      <Link to={`/country/${country_id}`} className={`btn ${styles.btnOutline}`}>More</Link>
     </div>
   )
 }
