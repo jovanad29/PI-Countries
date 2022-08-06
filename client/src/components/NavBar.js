5537bfeede5a3f7c075fd07c59a894a983f957e9
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-05 20:10:16 -0400

    Add style

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,17 +1,15 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../index.css'
-import '../assets/css/NavBar.css'
+import SearchBar from './SearchBar'
+import styles from '../assets/css/NavBar.module.css'
 
 function NavBar() {
   return (
     <header>
-        <div id='app-title'>Countries' App</div>
-        <nav>
-            <Link to='/home' className='btn btn-secondary'><li>Home</li></Link>
-            <Link to='/activity/create' className='btn btn-primary'><li>Add Activity</li></Link>
-        </nav>
+      <div id={styles.appTitle}>Countries' App</div>
+      <SearchBar />
+      <Link to='/activity/create' className='btn btn-primary'><li>Add Activity</li></Link>
     </header>
   )
 }
