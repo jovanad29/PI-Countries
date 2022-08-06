7cf56854d6f518490e8d16894c7cfa372f943f7d
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-05 20:10:57 -0400

    Add get countries by name reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
-import { GET_ALL_COUNTRIES, CREATE_ACTIVITY } from "../actions"; //GET_COUNTRY_DETAILS
+import {
+    GET_ALL_COUNTRIES,
+    GET_COUNTRIES_BY_NAME,
+    CREATE_ACTIVITY } from "../actions"; //GET_COUNTRY_DETAILS
 
 const initialState = {
+    allCountries: [],
     countries: [],
     activity: {},
     activities: []
@@ -9,6 +13,11 @@ const initialState = {
 const rootReducer = (state = initialState, { type, payload }) => {
     switch (type) {
         case GET_ALL_COUNTRIES:
+            return {
+                ...JSON.parse(JSON.stringify(state)),
+                allCountries: payload
+            }
+        case GET_COUNTRIES_BY_NAME:
             return {
                 ...JSON.parse(JSON.stringify(state)),
                 countries: payload
