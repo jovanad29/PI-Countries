a19366a546f8d4f9bc9d34ea273cb0d8076a8645
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-05 20:07:31 -0400

    Add getCountriesByName

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,17 +1,29 @@
 
 import axios from 'axios'
 
-export const GET_ALL_COUNTRIES = "GET_ALL_COUNTRIES"
-// export const GET_COUNTRY_DETAILS = "GET_COUNTRY_DETAILS"
-export const GET_ALL_ACTIVITIES = "GET_ALL_ACTIVITIES"
-export const CREATE_ACTIVITY = "CREATE_ACTIVITY"
+export const GET_ALL_COUNTRIES = 'GET_ALL_COUNTRIES'
+export const GET_COUNTRIES_BY_NAME = 'GET_COUNTRIES_BY_NAME'
+// export const GET_COUNTRY_DETAILS = 'GET_COUNTRY_DETAILS'
+export const GET_ALL_ACTIVITIES = 'GET_ALL_ACTIVITIES'
+export const CREATE_ACTIVITY = 'CREATE_ACTIVITY'
 
 
 export const getCountries = () => {
     return async (dispatch) => {
         try {
-            const { data } = await axios.get("http://localhost:3001/countries")
+            const { data } = await axios.get('http://localhost:3001/countries')
             return dispatch({ type: GET_ALL_COUNTRIES, payload: data })            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export const getCountriesByName = (searched) => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get('http://localhost:3001/countries?name='+searched)
+            return dispatch({ type: GET_COUNTRIES_BY_NAME, payload: data })            
         } catch (error) {
             console.log(error)
         }
@@ -21,7 +33,7 @@ export const getCountries = () => {
 // export const getCountryDetail = (id) => {
 //     return async (dispatch) => {
 //         try {
-//             const { data } = await axios.get("http://localhost:3001/countries/"+id)
+//             const { data } = await axios.get('http://localhost:3001/countries/'+id)
 //             return dispatch({ type: GET_COUNTRY_DETAILS, payload: data })            
 //         } catch (error) {
 //             console.log(error)
@@ -41,29 +53,29 @@ export function createActivity(payload) {
 };
 
 // export function getMoviesFavorite(payload) {
-//     return { type: "ADD_MOVIE_FAVORITE", payload };
+//     return { type: 'ADD_MOVIE_FAVORITE', payload };
 // }
 
 // export function removeMovieFavorite(payload) {
-//     return { type: "REMOVE_MOVIE_FAVORITE", payload };
+//     return { type: 'REMOVE_MOVIE_FAVORITE', payload };
 // }
    
 // export function getMovies(titulo) {
 //     return function(dispatch) {
-//       return fetch("http://www.omdbapi.com/?apikey=20dac387&s=" + titulo)
+//       return fetch('http://www.omdbapi.com/?apikey=20dac387&s=' + titulo)
 //         .then(response => response.json())
 //         .then(json => {
-//           dispatch({ type: "GET_MOVIES", payload: json });
+//           dispatch({ type: 'GET_MOVIES', payload: json });
 //         });
 //     };
 // }
   
 // export function getDetails(id) {
 //     return function(dispatch) {
-//       return fetch("http://www.omdbapi.com/?apikey=20dac387&i=" + id)
+//       return fetch('http://www.omdbapi.com/?apikey=20dac387&i=' + id)
 //         .then(response => response.json())
 //         .then(json => {
-//           dispatch({ type: "GET_DETAILS", payload: json });
+//           dispatch({ type: 'GET_DETAILS', payload: json });
 //         });
 //     };
 // }
