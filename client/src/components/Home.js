e85769ed251dcb8f501eb0c7c1cd4298ceeb3321
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-05 20:09:45 -0400

    Change to global state insted of local

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,45 +1,32 @@
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCountries } from '../actions'
 import NavBar from './NavBar'
 import CountryCard from './CountryCard'
-import SearchBar from './SearchBar'
-import '../assets/css/Home.css'
-import Sort from './Sort'
-import Filters from './Filters'
-import axios from 'axios'
+import styles from '../assets/css/Home.module.css'
 
 const Home = () => {
-  const allCountries = useSelector(state => state.countries)
+  const { allCountries, countries } = useSelector((state) => ({ 
+    allCountries: state.allCountries,
+    countries: state.countries
+  }))
   const dispatch = useDispatch()
-  const [countries, setCountries] = useState([])
   useEffect(() => {
     dispatch(getCountries())
   },[dispatch])
-  const onSearch = async (searched) => {
-    const { data } = await axios.get('http://localhost:3001/countries?name='+searched) 
-    setCountries(data)
-  }
   return (
     <>
       <NavBar />
-      <div className='container'>
-        <div className='filters-container'>
-          <SearchBar onSearch={onSearch}/>
-          <Sort />
-          <Filters />
-        </div>
-        <div className='cards-container'>
-            {countries.length ?
-              countries.map((c) => {
-                return <CountryCard key={c.country_id} {...c} />
-              }).slice(0,9) :
-              allCountries.map((c) => {
-                return <CountryCard key={c.country_id} {...c} />
-              }).slice(0,9)
-            }
-        </div>
+      <div className={styles.container}>
+        {countries.length ?
+          countries.map((c) => {
+            return <CountryCard key={c.country_id} {...c} />
+          }) :
+          allCountries.map((c) => {
+            return <CountryCard key={c.country_id} {...c} />
+          })
+        }
       </div>
     </>
   )
