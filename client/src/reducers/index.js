8f3bcb9fea41948c3287f5368636aaacb7f95151
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-07 18:47:17 -0400

    Add filters and error handling to reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,42 +1,75 @@
 import {
     GET_ALL_COUNTRIES,
     GET_COUNTRIES_BY_NAME,
-    CREATE_ACTIVITY } from "../actions"; //GET_COUNTRY_DETAILS
+    CREATE_ACTIVITY, 
+    FILTER_BY_CRITERIA,
+    GET_ALL_ACTIVITIES} from "../actions";
 
 const initialState = {
     countries: [],
+    filteredCountries: [],
+    activities: [],
     activity: {},
-    activities: []
+    error: {}
   };
 
-const rootReducer = (state = initialState, { type, payload }) => {
-    switch (type) {
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
         case GET_ALL_COUNTRIES:
             return {
                 ...JSON.parse(JSON.stringify(state)),
-                countries: payload
+                countries: action.payload,
+                filteredCountries: []
             }
         case GET_COUNTRIES_BY_NAME:
             return {
                 ...JSON.parse(JSON.stringify(state)),
-                countries: payload
+                countries: action.payload,
+                error: action.error ? {
+                    status: action.error.response.status,
+                    message: action.error.response.data.error.message
+                } : {},
+                filteredCountries: []
             }
-        // case GET_COUNTRY_DETAILS:
-        // return {
-        //     ...JSON.parse(JSON.stringify(state)),
-        //     country: payload
-        // }
-        case CREATE_ACTIVITY:
-            console.log("reducer", payload)
+        case FILTER_BY_CRITERIA:
+            let filtered = []
+            let isFiltering = false
+            if (action.values.continent && action.values.continent !== '0'){
+                isFiltering = true
+                filtered = filtered.length ? filtered : [...state.countries]
+                filtered = [...state.countries].filter(c => {
+                    return c.continent === action.values.continent
+                })
+            }
+            if (action.values.activity && action.values.activity !== '0'){
+                isFiltering = true
+                filtered = filtered.length ? filtered : [...state.countries]
+                filtered = filtered.filter(c => {
+                    return c.activities.some(a => a.activity_id === action.values.activity)
+                })
+            }
             return {
                 ...JSON.parse(JSON.stringify(state)),
-                activity:payload
+                filteredCountries: filtered,
+                error: isFiltering && !filtered.length ? {
+                    message: "No countries found under selected criteria"
+                } : {}
             }
-        // case DELETE_PRODUCT:
-        // return {
-        //     ...state,
-        //     products: [...state.products].filter((product) => product.id !== action.payload)
-        // }
+        case CREATE_ACTIVITY:
+            return {
+                ...JSON.parse(JSON.stringify(state)),
+                activity: action.payload
+            }
+        case GET_ALL_ACTIVITIES:
+            return {
+                ...JSON.parse(JSON.stringify(state)),
+                activities: action.payload,
+                error: action.error ? {
+                    status: action.error.response.status,
+                    message: action.error.response.data.error.message
+                } : {},
+                filteredCountries: []
+            }
         default: return state
     }
 };
