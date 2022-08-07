d410fef2adbb4b2808a5b898be3bc66598eca2f9
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-07 18:44:45 -0400

    Add errors to actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 export const GET_ALL_COUNTRIES = 'GET_ALL_COUNTRIES'
 export const GET_COUNTRIES_BY_NAME = 'GET_COUNTRIES_BY_NAME'
+export const FILTER_BY_CRITERIA = 'FILTER_BY_CRITERIA'
 // export const GET_COUNTRY_DETAILS = 'GET_COUNTRY_DETAILS'
 export const GET_ALL_ACTIVITIES = 'GET_ALL_ACTIVITIES'
 export const CREATE_ACTIVITY = 'CREATE_ACTIVITY'
@@ -14,7 +15,7 @@ export const getCountries = () => {
             const { data } = await axios.get('http://localhost:3001/countries')
             return dispatch({ type: GET_ALL_COUNTRIES, payload: data })            
         } catch (error) {
-            console.log(error)
+            return dispatch({ type: GET_ALL_COUNTRIES, payload: [], error })
         }
     }
 }
@@ -25,21 +26,16 @@ export const getCountriesByName = (searched) => {
             const { data } = await axios.get('http://localhost:3001/countries?name='+searched)
             return dispatch({ type: GET_COUNTRIES_BY_NAME, payload: data })            
         } catch (error) {
-            console.log(error)
+            return dispatch({ type: GET_COUNTRIES_BY_NAME, payload: [], error })
         }
     }
 }
 
-// export const getCountryDetail = (id) => {
-//     return async (dispatch) => {
-//         try {
-//             const { data } = await axios.get('http://localhost:3001/countries/'+id)
-//             return dispatch({ type: GET_COUNTRY_DETAILS, payload: data })            
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     }
-// }
+export const filterByCriteria = (values) => {
+    return (dispatch) => {
+        return dispatch({type: FILTER_BY_CRITERIA, values})
+    }
+}
 
 export function createActivity(payload) {
     return async (dispatch) => {
@@ -47,36 +43,18 @@ export function createActivity(payload) {
             var { data } = await axios.post('http://localhost:3001/activities', payload)
             return dispatch({ type: CREATE_ACTIVITY, payload: data })
         } catch (error) {
-            console.log(error)
+            return dispatch({ type: GET_COUNTRIES_BY_NAME, payload: error.data.values, error })
         };
     };
 };
 
-// export function getMoviesFavorite(payload) {
-//     return { type: 'ADD_MOVIE_FAVORITE', payload };
-// }
-
-// export function removeMovieFavorite(payload) {
-//     return { type: 'REMOVE_MOVIE_FAVORITE', payload };
-// }
-   
-// export function getMovies(titulo) {
-//     return function(dispatch) {
-//       return fetch('http://www.omdbapi.com/?apikey=20dac387&s=' + titulo)
-//         .then(response => response.json())
-//         .then(json => {
-//           dispatch({ type: 'GET_MOVIES', payload: json });
-//         });
-//     };
-// }
-  
-// export function getDetails(id) {
-//     return function(dispatch) {
-//       return fetch('http://www.omdbapi.com/?apikey=20dac387&i=' + id)
-//         .then(response => response.json())
-//         .then(json => {
-//           dispatch({ type: 'GET_DETAILS', payload: json });
-//         });
-//     };
-// }
-
+export const getActivities = () => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get('http://localhost:3001/activities')
+            return dispatch({ type: GET_ALL_ACTIVITIES, payload: data })            
+        } catch (error) {
+            return dispatch({ type: GET_ALL_ACTIVITIES, payload: [], error })
+        }
+    }
+}
