e1c7247954faad8799ed222cff5fb90709dc428a
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-07 18:46:38 -0400

    Add filters and pagination

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,28 +1,43 @@
 
-import React, { useEffect} from 'react'
+import React, { useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCountries } from '../actions'
 import NavBar from './NavBar'
+import Filters from './Filters'
 import CountryCard from './CountryCard'
+import Pagination from './Pagination'
 import styles from '../assets/css/Home.module.css'
 
 const Home = () => {
-  const { countries } = useSelector((state) => ({
-    countries: state.countries
+  const { countries, filtered, error } = useSelector((state) => ({
+    countries: state.countries,
+    filtered: state.filteredCountries,
+    error: state.error
   }))
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(getCountries())
+    dispatch(getCountries())    
   },[dispatch])
+  const whatCountries = !filtered.length && !Object.keys(error).length ? countries : filtered
+  const [nPage, setPage] = useState(1)
+  const perPage = nPage === 1 ? 9 : 10
+  let nPages = Math.ceil(whatCountries.length / perPage)
   return (
     <>
       <NavBar />
+      <Filters />
+      {/* <Pagination nPage={nPage} setPage={setPage} nPages={nPages} /> */}
       <div className={styles.container}>
-        {countries?.map((c) => {
+        {
+          whatCountries.slice(((nPage - 1) * perPage), (((nPage - 1) * perPage) + perPage)).map((c) => {
             return <CountryCard key={c.country_id} {...c} />
           })
         }
+        <div style={{flexDirection:'column'}}>
+          <h3>{error.message}</h3>
+        </div>
       </div>
+      <Pagination nPage={nPage} setPage={setPage} nPages={nPages} />
     </>
   )
 }
