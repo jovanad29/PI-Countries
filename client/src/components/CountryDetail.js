e60c3539e709ace0b9d9521118a5eb03a30d5f19
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-07 21:37:57 -0400

    Add classes for styling

--- client/src/components/CountryDetail.js
+++ client/src/components/CountryDetail.js
@@ -8,6 +8,7 @@ import NavBar from './NavBar'
 const CountryDetail = () => {
   const { id } = useParams()
   const [detail, setDetail] = useState({})
+  const difficulties = {1:'Peaceful', 2:'Easy', 3:'Normal', 4:'Hard', 5:'Professional'}
   useEffect(()=>{
     axios.get("http://localhost:3001/countries/"+id).then(
       (res) => {
@@ -19,31 +20,45 @@ const CountryDetail = () => {
   return (
     <>
       <NavBar />
-      <div className='container'>
-        <div className='country-container'>
-          {Object.keys(detail).length ?
-            <>
-              <h1>{detail.name}</h1>
-              <img src={detail.flag_img[ 1 ]} alt='country flag' />
+      <main className='container'>
+        {Object.keys(detail).length ?
+          <div className='details-container'>
+          <h1>{detail.name}</h1>
+          <div className='details-wrapper'>
+            <div className='box'>
+              <img src={detail.flag_img[ 0 ]} alt='country flag' width={480} />
               <p>COD: {detail.country_id}</p>
               <p>CAPITAL: {detail.capital.join(', ')}</p>
               <p>CONTINENT: {detail.continent}</p>
-              <p>SUBREGION: {detail.subregion ? detail.subregion : ''}</p>
-              <p>AREA: {detail.area}km2</p>
+              <p>SUBREGION: {detail.subregion ? detail.subregion : 'N/A'}</p>
+              <p>AREA: {detail.area} km<sup>2</sup></p>
               <p>POPULATION: {detail.population}</p>
-              <ul>ACTIVITIES:{detail.activities.length ? 
-                detail.activities.map(a => {
-                  return <li key={a.activity_id}>{a.name}</li>
-                }) : 
+            </div>
+            <div className='box'>
+              <h2>ACTIVITIES</h2>
+              <ul>
+                {detail.activities.length ? 
+                  detail.activities.map(a => {
+                    return (
+                    <div className='activity'  key={Math.random()}>
+                      <h3 key={a.activity_id}>{a.name}</h3>
+                      <ul key={Math.random()}>
+                        <li key={Math.random()}>Difficulty: {difficulties[a.difficulty]}</li>
+                        <li key={Math.random()}>Duration: {a.duration} hours</li>
+                        <li key={Math.random()}>Season: {a.season}</li>
+                      </ul>
+                    </div>
+                    )
+                  }) : 
                 <li>No activities registered</li>}
               </ul>
-            </>
-          
-          : ''
-          }
-          <Link to='/home' className='btn btn-secondary'>Volver a home</Link> {/* goBack? */}
+            </div>
+          </div>     
         </div>
-      </div>
+        : ''
+        }
+        <Link to='/home' className='btn btn-secondary'>Back to Home</Link>
+      </main>
     </>
   )
 }
