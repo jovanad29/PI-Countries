4f701d1c3c5386a9cf2e2df7c64dae393b94d322
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-08-07 20:12:21 -0400

    Add last page

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import styles from '../assets/css/Pagination.module.css'
 
-function Pagination({nPage, setPage, nPages}) {
+function Pagination({nPage, setPage, nPages, lPage}) {
     const nextPage = () => {
         if (nPage < nPages) {
             setPage(nPage + 1)
@@ -15,14 +15,26 @@ function Pagination({nPage, setPage, nPages}) {
         }
         return false
     }
+    const firstPage = () => {
+        setPage(1)
+    }
+    const lastPage = () => {
+        setPage(lPage)
+    }
     return (
         <div className={styles.container}>
+            <button className={styles.doubleArrow} onClick={firstPage}>
+                {"<<"}
+            </button>
             <button onClick={prevPage}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="arcs"><path d="M15 18l-6-6 6-6"></path></svg>
             </button>
-            <p>{nPage} / {nPages}</p>
+            <span>{nPage} / {nPages}</span>
             <button onClick={nextPage}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="arcs"><path d="M9 18l6-6-6-6"></path></svg>
+            </button>
+            <button className={styles.doubleArrow} onClick={lastPage}>
+                {">>"}
             </button>
         </div>
     )
