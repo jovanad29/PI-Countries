3139bd077edcbdd1d11339b847d080444d734b13
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-10-02 01:54:33 -0400

    Remove searchbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,17 +1,15 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import SearchBar from '../SearchBar/SearchBar'
 import styles from './NavBar.module.css'
 
 function NavBar() {
-  return (
-    <header>
-      <Link to='/'><div id={styles.appTitle}>Countries' App</div></Link>
-      <SearchBar />
-      <Link to='/activity/create' className='btn btn-primary'><li>Add Activity</li></Link>
-    </header>
-  )
+	return (
+		<header>
+			<Link to='/'><div id={styles.appTitle}>Countries' App</div></Link>
+			<Link to='/activity/create' className='btn btn-primary'>Add Activity</Link>
+		</header>
+	)
 }
 
 export default NavBar
