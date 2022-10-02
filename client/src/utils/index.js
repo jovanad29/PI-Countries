3198e88a6a124ae83bb650a5b4546a0eaee1c5a7
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-10-02 01:55:04 -0400

    Remove timeout

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const fetch = axios.create({
     baseURL: process.env.REACT_APP_API || 'http://localhost:3001',
-    timeout: 1500
+    // timeout: 1500
 });
 // instance.defaults.headers.common['x-api-key'] = process.env.API_KEY;
 module.exports = fetch
