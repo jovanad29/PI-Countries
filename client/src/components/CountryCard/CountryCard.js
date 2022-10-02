df7037a47d0710f1941effb803f77d3b2beb442f
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-10-02 01:50:23 -0400

    Add title

--- client/src/components/CountryCard/CountryCard.js
+++ client/src/components/CountryCard/CountryCard.js
@@ -6,13 +6,18 @@ import styles from './CountryCard.module.css'
 
 const CountryCard = (props) => {
 	const { country_id, name, continent, flag_img, population } = props
+	const tagName = name.length > 18
+		?
+		<h3 style={{ fontSize: '1.8rem', marginBottom: '1.15em' }}>{name}</h3>
+		:
+		<h3>{name}</h3>
 	return (
 		<Link to={`/country/${country_id}`}>
 			<section className={styles.card}>
 				<figure>
 					<img src={flag_img[ 1 ]} alt='country flag' />
 				</figure>
-				<h3>{name.length > 19 ? name.slice(0, 20) + '...' : name}</h3>
+				{tagName}
 				<div className={styles.description}>
 					<p><strong>Continent: </strong>{continent}</p>
 					<p><strong>Population: </strong>{new Intl.NumberFormat('en-US').format(population)}</p>
