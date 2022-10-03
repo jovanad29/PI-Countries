c261c06ba4a4c6e2a80c7cbed360d946af47d082
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-10-03 00:34:44 -0400

    Add icon to seasons

--- api/syncDB/syncdb.js
+++ api/syncDB/syncdb.js
@@ -1,9 +1,9 @@
 
 const { conn } = require('../src/db.js');
-const { Country, Season } = require('../src/db') 
+const { Country, Season } = require('../src/db')
 const axios = require('axios');
 
-conn.sync({ force: true }).then( async () => {
+conn.sync({ force: true }).then(async () => {
     try {
         const { data } = await axios.get('https://restcountries.com/v3/all')
         const countries = data.map(c => {
@@ -11,26 +11,31 @@ conn.sync({ force: true }).then( async () => {
                 country_id: c.cca3,
                 name: c.name.common,
                 flag_img: c.flags,
-                continent: c.continents[0],
-                capital: c.capital ? c.capital : ["N/A"], // some are undefined
+                continent: c.continents[ 0 ],
+                capital: c.capital ? c.capital : [ "N/A" ], // some are undefined
                 subregion: c.subregion, // some are undefined
                 area: c.area,
                 population: c.population
             }
         })
-        await Country.bulkCreate(countries)        
+        await Country.bulkCreate(countries)
     } catch (error) {
         console.log("Error while adding countries")
         console.log(error)
-    } finally{
+    } finally {
         console.log(`${await Country.count()} countries added`)
     }
     try {
-        const seasons = ['Summer', 'Fall', 'Winter', 'Spring']
-        seasons.forEach(async s => await Season.create({name: s}))
+        const seasons = [
+            { name: 'Summer', icon: 'https://i.ibb.co/9Ym2qRm/sun.png' },
+            { name: 'Fall', icon: 'https://i.ibb.co/1Jjvwjq/autumn-tree-leaves.png' },
+            { name: 'Winter', icon: 'https://i.ibb.co/Y7wRg9V/snowflake.png' },
+            { name: 'Spring', icon: 'https://i.ibb.co/tzR6Y24/sakura.png' }
+        ]
+        seasons.forEach(async s => await Season.create({ name: s.name, icon: s.icon }))
     } catch (error) {
         console.log("Error while adding seasons")
-        console.log(error)        
+        console.log(error)
     }
     console.log('End of synchronization')
 }).catch(e => {
