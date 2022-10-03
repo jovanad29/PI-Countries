1e28d139d38db4ecb4a2e2e69b07a81652f0fc7b
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-10-03 00:39:57 -0400

    Add styles to nav title

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -2,11 +2,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './NavBar.module.css'
+import { BiWorld } from 'react-icons/bi'
 
 function NavBar() {
 	return (
 		<header>
-			<Link to='/'><div id={styles.appTitle}>Countries' App</div></Link>
+			<Link to='/'><div id={styles.appTitle}><BiWorld /><p>Henry Countries</p></div></Link>
 			<Link to='/activity/create' className='btn btn-primary'>Add Activity</Link>
 		</header>
 	)
