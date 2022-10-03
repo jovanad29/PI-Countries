9f57d5ae7b6d0dcfd9046f44f9159cd0c583eaec
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-10-03 00:33:54 -0400

    Remove comment

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -2,29 +2,29 @@ const { DataTypes } = require('sequelize');
 
 
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('activity', {
-    activity_id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      unique: true,
-      defaultValue: DataTypes.UUIDV4
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        is: /^[ a-zA-ZÀ-ÿ\u00f1\u00d1]*$/g
-      }
-    },
-    difficulty: {
-        type: DataTypes.ENUM(["1", "2", "3", "4", "5"]),
-        allowNull: false
-    },
-    duration: { // en días? en horas?
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
-  })
+	// defino el modelo
+	sequelize.define('activity', {
+		activity_id: {
+			type: DataTypes.UUID,
+			primaryKey: true,
+			unique: true,
+			defaultValue: DataTypes.UUIDV4
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				is: /^[ a-zA-ZÀ-ÿ\u00f1\u00d1]*$/g
+			}
+		},
+		difficulty: {
+			type: DataTypes.ENUM([ "1", "2", "3", "4", "5" ]),
+			allowNull: false
+		},
+		duration: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		}
+	})
 };
