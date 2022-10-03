f50bfc534b71781a7ac68b67c5264d403ca53037
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-10-03 00:39:39 -0400

    Add blur effect when rendering

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -8,15 +8,23 @@ import CountryCard from '../CountryCard/CountryCard'
 import Pagination from '../Pagination/Pagination'
 import styles from './Home.module.css'
 import Loader from '../Loader/Loader'
+import { IoArrowUp, IoArrowDown } from "react-icons/io5";
 
 const Home = () => {
 	const { countries, error } = useSelector((state) => ({
 		countries: state.countries,
 		error: state.error
 	}))
+	const [ filterIsActive, setFilterIsActive ] = useState(false);
 	const dispatch = useDispatch()
 	useEffect(() => {
+		document.getElementById('app').classList.add('filter')
+		document.getElementById('app').classList.add('border')
 		dispatch(getCountries())
+		return () => {
+			document.getElementById('app').classList.remove('filter')
+			document.getElementById('app').classList.remove('border')
+		}
 	}, [ dispatch ])
 	const [ nPage, setPage ] = useState(1)
 	const perPage = 8
@@ -25,25 +33,36 @@ const Home = () => {
 		setPage(1)
 	}, [ countries.length ])
 	let shownCountries = countries.slice(((nPage - 1) * perPage), (((nPage - 1) * perPage) + perPage))
+	if (!countries.length && !Object.keys(error).length) return (<><NavBar /> <Loader /></>)
 	return (
 		<>
 			<NavBar />
-			<Filters />
+			<div className={styles.filtersContainer}>
+				<div className={styles.wrapper}>
+					<div>
+						<p>{filterIsActive ? 'Hide' : 'Show'} Filters</p>
+					</div>
+					<button onClick={() => setFilterIsActive(!filterIsActive)} className={styles.toggleBtn}>
+						{filterIsActive ? <IoArrowUp /> : <IoArrowDown />}
+					</button>
+				</div>
+			</div>
+			{filterIsActive && <Filters />}
 			<div className={styles.container}>
 				<Pagination nPage={nPage} setPage={setPage} nPages={nPages} />
 				<div className={styles.cardsContainer}>
 					{
-						shownCountries.length ?
+						shownCountries.length
+							?
 							shownCountries.map((c) => {
 								return <CountryCard key={c.country_id} {...c} />
-							}) : Object.keys(error).length ?
-								<div className={styles.errorMsg}>
-									<h3>{error.message}</h3>
-								</div> :
-								<Loader />
+							})
+							: <div className={styles.errorMsg}>
+								<h3>{error.message}</h3>
+							</div>
 					}
 				</div>
-				<Pagination nPage={nPage} setPage={setPage} nPages={nPages} />
+				{/* <Pagination nPage={nPage} setPage={setPage} nPages={nPages} /> */}
 			</div>
 		</>
 	)
