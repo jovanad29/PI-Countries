ce2e149d2bfe262b1790fb6f4366ca4f424909b9
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-10-03 00:34:29 -0400

    Add icon field

--- api/src/models/Season.js
+++ api/src/models/Season.js
@@ -2,21 +2,28 @@ const { DataTypes } = require('sequelize');
 
 
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('season', {
-    season_id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      unique: true,
-      defaultValue: DataTypes.UUIDV4
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        is: /^[ a-zA-ZÀ-ÿ\u00f1\u00d1]*$/g
-      }
-    }
-  })
+	// defino el modelo
+	sequelize.define('season', {
+		season_id: {
+			type: DataTypes.UUID,
+			primaryKey: true,
+			unique: true,
+			defaultValue: DataTypes.UUIDV4
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				is: /^[ a-zA-ZÀ-ÿ\u00f1\u00d1]*$/g
+			}
+		},
+		icon: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isUrl: true
+			}
+		}
+	})
 };
