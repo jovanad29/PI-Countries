67de956b3e625a234c29ac88221a366dd6222b5b
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-10-03 06:34:15 -0400

    Refactor

--- client/src/components/CountryCard/CountryCard.js
+++ client/src/components/CountryCard/CountryCard.js
@@ -6,11 +6,11 @@ import styles from './CountryCard.module.css'
 
 const CountryCard = (props) => {
 	const { country_id, name, continent, flag_img, population, capital } = props
-	const tagName = name.length > 16
+	const tagName = name.length > 15
 		?
-		<h3 style={{ fontSize: '1.6rem', marginBottom: '1.15em' }}>{name}</h3>
+		<h2 className={styles.cardTitle}>{name.slice(0, 15) + '...'}</h2>
 		:
-		<h3>{name}</h3>
+		<h2 className={styles.cardTitle}>{name}</h2>
 	return (
 		<Link to={`/country/${country_id}`}>
 			<section className={styles.card}>
@@ -20,7 +20,7 @@ const CountryCard = (props) => {
 				{tagName}
 				<div className={styles.description}>
 					<p><strong>Continent: </strong>{continent}</p>
-					<p><strong>Capital: </strong>{capital}</p>
+					<p><strong>Capital: </strong>{capital.join(', ')}</p>
 					<p><strong>Population: </strong>{new Intl.NumberFormat('en-US').format(population)}</p>
 				</div>
 			</section>
